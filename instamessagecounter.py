377f816b2dfcd72136e8102d76effab7da81198f
Preetham Thelluri <46249739+preethampython101@users.noreply.github.com>
2021-02-24 20:13:23 -0500

    Update instamessagecounter.py

--- instamessagecounter.py
+++ instamessagecounter.py
@@ -1,17 +1,34 @@
+#Importing the module to read the path names of all your files containing your chats
 import sys
 #Greeting the user
-print("""A warm welcome to the Instagram Chat Message Counter.\nThis app will tell you the number of messages you have exchanged in specific chats.""")
+print("""A warm welcome to the Instagram Chat Message Counter.\nThis program will tell you the number of messages you have exchanged among all chats including requests.\nThe input is a file containing all the absolute pathnames of your chat files.""")
 #Opening the file with read access
 f = open(sys.argv[1], "r")
 #Assigning the contents of the file to a variable "g"
 g = f.readlines()
-#Creating an initial counter variable
-c = 0
+#A counter variable to count the toal messages
+mess = 0
 #Iterating through the lines of the code to find a specific sequence
 for i in g:
-    #The next statement is optional, but it helps to make the data cryptic and challenging to read
-    print(i[::-1].swapcase().replace("NAM", "###").split())
-    #Incrementing the message count
-    c += i.count("sender_name")
-#Printing out the final number of messages 
-print("There are", c, "messages between you and the interested party!")
+    #Getting rid of the new line characters
+    i = i.replace("\n", "").replace('"', "")
+    #Opening a file given the path
+    j = open(i, "r")
+    #Reading each individual line and assigning to a variable "k"
+    k = j.readlines()
+    #Creating an initial counter variable
+    c = 0
+    #Iterating through the lines of the code to find a specific sequence
+    for l in k:
+        #The next statement is optional, but it helps to make the data cryptic and challenging to read
+        print(l[::-1].swapcase().replace("N", "$").replace("A", "#").split())
+        #Incrementing the message count for the specific chat
+        c += l.count("sender_name")
+        #Incrementing the message count for all chats thus far
+        mess += l.count("sender_name")
+    #Printing out the final number of messages 
+    print("There are", c, "messages between you and the interested party!")
+    #Printing blank line
+    print("\n")
+#Printing all the messages, adding all the chats and requests together!
+print("You have", mess, "total sent and received messages on Instagram, including requests!")
